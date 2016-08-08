7d4b5cb0db0277c77c5dbf7d38f416633aca1810
robsonrosa <robsonrosa@outlook.com>
2016-08-07 22:47:24 -0300

    First typescript code! Here we go!!

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+interface IPerson {
+   firstName: string;
+   lastName: string;
+}
+
+abstract class Person implements IPerson {
+   firstName: string;
+   lastName: string;
+
+   constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+   }
+
+   abstract getTitle(): string;
+
+   greet() {
+      return `Hello! My name is ${this.firstName} ${this.lastName} and I'm a ${this.getTitle()}.`;
+   }
+}
+
+class Student extends Person {
+   constructor(firstName: string, lastName: string) {
+      super(firstName, lastName);
+   }
+
+   getTitle() {
+      return 'student';
+   }
+}
+
+class Professor extends Person {
+
+   constructor(firstName: string, lastName: string) {
+      super(firstName, lastName);
+   }
+
+   getTitle() {
+      return 'professor';
+   }
+}
+
+let student = new Student('Alan', 'Turing'),
+   professor = new Professor('Albert', 'Eistein');
+
+console.info(student.greet());
+console.info(professor.greet());
